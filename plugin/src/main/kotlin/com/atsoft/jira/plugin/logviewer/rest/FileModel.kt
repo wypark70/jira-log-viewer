f7779ff1ad8578c7feee079b134fb1ef5b0cd5df
W.Y.Park <wypark70@gmail.com>
2026-01-28 05:32:00 +0900

    refactor: cleanup build env

--- plugin/src/main/kotlin/com/atsoft/jira/plugin/logviewer/rest/FileModel.kt
+++ plugin/src/main/kotlin/com/atsoft/jira/plugin/logviewer/rest/FileModel.kt
@@ -1,7 +1,7 @@
 package com.atsoft.jira.plugin.logviewer.rest
 
-import javax.xml.bind.annotation.XmlElement
-import javax.xml.bind.annotation.XmlRootElement
+import jakarta.xml.bind.annotation.XmlElement
+import jakarta.xml.bind.annotation.XmlRootElement
 
 @XmlRootElement
 data class FileModel(
